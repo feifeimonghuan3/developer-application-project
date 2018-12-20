bed579e3f9c863481312d5bd0cf6ebf5d603827c
林润佳 <john.lin@antcosa.com>
2018-12-20 18:07:27 +0800

    练习 startActivityForResult

--- app/src/main/java/com/example/linrunjia/developerapplicationproject/DisplayMessageActivity.java
+++ app/src/main/java/com/example/linrunjia/developerapplicationproject/DisplayMessageActivity.java
@@ -3,6 +3,7 @@ package com.example.linrunjia.developerapplicationproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -16,5 +17,12 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.textView1);
         textView.setText(message);
+    }
+
+    public void back(View view) {
+        Intent intent1 = new Intent();
+        intent1.putExtra("result", "你搜索到并选择的数据");
+        this.setResult(1, intent1);
+        this.finish();
     }
 }
--- app/src/main/java/com/example/linrunjia/developerapplicationproject/MainActivity.java
+++ app/src/main/java/com/example/linrunjia/developerapplicationproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.linrunjia.developerapplicationproject.MESSAGE";
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("hayashi", "hayashi");
     }
 
     public void sendMessage(View view) {
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String req = requestCode + "";
+        Log.i("hayashi", req);// 回的数据
+        super.onActivityResult(requestCode, resultCode, data);
+        String result = data.getStringExtra("result"); //得到新Activity 关闭后返
+        Log.i("hayashi", result);// 回的数据
     }
 }
